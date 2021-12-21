591ff1aa56f8695152fa1dbcab0f48cbdf99b149
张翠山 <290794272@qq.com>
2021-12-21 10:08:52 +0800

    再次引入本地静态资源?

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -98,7 +98,7 @@ export default {
   proxy: {
     // Use a fake value for use at build-time
     '/v1/': {
-      target: process.env.API_URL || 'http://127.0.0.1:8000'
+      target: process.env.API_URL || 'http://118.195.181.130:8000'
     }
   },
   /*
